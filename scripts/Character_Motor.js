2fa54638484d89c92d3af18b54d7946d4fdeffec
Esat Taha <esat.taha.ibis@gmail.com>
2013-12-27 13:59:29 +0200

    Final Form of character motor

--- scripts/Character_Motor.js
+++ scripts/Character_Motor.js
@@ -383,3 +383,205 @@ private function ApplyInputVelocityChange (velocity : Vector3) {
 	
     return velocity;
 }
+
+
+    private function ApplyGravityAndJumping (velocity : Vector3) {
+	
+        if (!inputJump || !canControl) {
+            jumping.holdingJumpButton = false;
+            jumping.lastButtonDownTime = -100;
+        }
+	
+        if (inputJump && jumping.lastButtonDownTime < 0 && canControl)
+            jumping.lastButtonDownTime = Time.time;
+	
+        if (grounded)
+            velocity.y = Mathf.Min(0, velocity.y) - movement.gravity * Time.deltaTime;
+        else {
+            velocity.y = movement.velocity.y - movement.gravity * Time.deltaTime;
+		
+            // When jumping up we don't apply gravity for some time when the user is holding the jump button.
+            // This gives more control over jump height by pressing the button longer.
+            if (jumping.jumping && jumping.holdingJumpButton) {
+                // Calculate the duration that the extra jump force should have effect.
+                // If we're still less than that duration after the jumping time, apply the force.
+                if (Time.time < jumping.lastStartTime + jumping.extraHeight / CalculateJumpVerticalSpeed(jumping.baseHeight)) {
+                    // Negate the gravity we just applied, except we push in jumpDir rather than jump upwards.
+                    velocity += jumping.jumpDir * movement.gravity * Time.deltaTime;
+                }
+            }
+		
+            // Make sure we don't fall any faster than maxFallSpeed. This gives our character a terminal velocity.
+            velocity.y = Mathf.Max (velocity.y, -movement.maxFallSpeed);
+        }
+		
+        if (grounded) {
+            // Jump only if the jump button was pressed down in the last 0.2 seconds.
+            // We use this check instead of checking if it's pressed down right now
+            // because players will often try to jump in the exact moment when hitting the ground after a jump
+            // and if they hit the button a fraction of a second too soon and no new jump happens as a consequence,
+            // it's confusing and it feels like the game is buggy.
+            if (jumping.enabled && canControl && (Time.time - jumping.lastButtonDownTime < 0.2)) {
+                grounded = false;
+                jumping.jumping = true;
+                jumping.lastStartTime = Time.time;
+                jumping.lastButtonDownTime = -100;
+                jumping.holdingJumpButton = true;
+			
+                // Calculate the jumping direction
+                if (TooSteep())
+                    jumping.jumpDir = Vector3.Slerp(Vector3.up, groundNormal, jumping.steepPerpAmount);
+                else
+                    jumping.jumpDir = Vector3.Slerp(Vector3.up, groundNormal, jumping.perpAmount);
+			
+                // Apply the jumping force to the velocity. Cancel any vertical velocity first.
+                velocity.y = 0;
+                velocity += jumping.jumpDir * CalculateJumpVerticalSpeed (jumping.baseHeight);
+			
+                // Apply inertia from platform
+                if (movingPlatform.enabled &&
+                    (movingPlatform.movementTransfer == MovementTransferOnJump.InitTransfer ||
+                    movingPlatform.movementTransfer == MovementTransferOnJump.PermaTransfer)
+                ) {
+                    movement.frameVelocity = movingPlatform.platformVelocity;
+                    velocity += movingPlatform.platformVelocity;
+                }
+			
+                SendMessage("OnJump", SendMessageOptions.DontRequireReceiver);
+            }
+            else {
+                jumping.holdingJumpButton = false;
+            }
+        }
+	
+        return velocity;
+    }
+
+        function OnControllerColliderHit (hit : ControllerColliderHit) {
+            if (hit.normal.y > 0 && hit.normal.y > groundNormal.y && hit.moveDirection.y < 0) {
+                if ((hit.point - movement.lastHitPoint).sqrMagnitude > 0.001 || lastGroundNormal == Vector3.zero)
+                    groundNormal = hit.normal;
+                else
+                    groundNormal = lastGroundNormal;
+		
+                movingPlatform.hitPlatform = hit.collider.transform;
+                movement.hitPoint = hit.point;
+                movement.frameVelocity = Vector3.zero;
+            }
+        }
+
+            private function SubtractNewPlatformVelocity () {
+                // When landing, subtract the velocity of the new ground from the character's velocity
+                // since movement in ground is relative to the movement of the ground.
+                if (movingPlatform.enabled &&
+                    (movingPlatform.movementTransfer == MovementTransferOnJump.InitTransfer ||
+                    movingPlatform.movementTransfer == MovementTransferOnJump.PermaTransfer)
+                ) {
+                    // If we landed on a new platform, we have to wait for two FixedUpdates
+                    // before we know the velocity of the platform under the character
+                    if (movingPlatform.newPlatform) {
+                        var platform : Transform = movingPlatform.activePlatform;
+                        yield WaitForFixedUpdate();
+                        yield WaitForFixedUpdate();
+                        if (grounded && platform == movingPlatform.activePlatform)
+                            yield 1;
+                    }
+                    movement.velocity -= movingPlatform.platformVelocity;
+                }
+            }
+
+            private function MoveWithPlatform () : boolean {
+                return (
+                    movingPlatform.enabled
+                    && (grounded || movingPlatform.movementTransfer == MovementTransferOnJump.PermaLocked)
+                    && movingPlatform.activePlatform != null
+                );
+            }
+
+                private function GetDesiredHorizontalVelocity () {
+                    // Find desired velocity
+                    var desiredLocalDirection : Vector3 = tr.InverseTransformDirection(inputMoveDirection);
+                    var maxSpeed : float = MaxSpeedInDirection(desiredLocalDirection);
+                    if (grounded) {
+                        // Modify max speed on slopes based on slope speed multiplier curve
+                        var movementSlopeAngle = Mathf.Asin(movement.velocity.normalized.y)  * Mathf.Rad2Deg;
+                        maxSpeed *= movement.slopeSpeedMultiplier.Evaluate(movementSlopeAngle);
+                    }
+                    return tr.TransformDirection(desiredLocalDirection * maxSpeed);
+                }
+
+                private function AdjustGroundVelocityToNormal (hVelocity : Vector3, groundNormal : Vector3) : Vector3 {
+                    var sideways : Vector3 = Vector3.Cross(Vector3.up, hVelocity);
+                    return Vector3.Cross(sideways, groundNormal).normalized * hVelocity.magnitude;
+                }
+
+                    private function IsGroundedTest () {
+                        return (groundNormal.y > 0.01);
+                    }
+
+                    function GetMaxAcceleration (grounded : boolean) : float {
+                        // Maximum acceleration on ground and in air
+                        if (grounded)
+                            return movement.maxGroundAcceleration;
+                        else
+                            return movement.maxAirAcceleration;
+                    }
+
+                        function CalculateJumpVerticalSpeed (targetJumpHeight : float) {
+                            // From the jump height and gravity we deduce the upwards speed 
+                            // for the character to reach at the apex.
+                            return Mathf.Sqrt (2 * targetJumpHeight * movement.gravity);
+                        }
+
+                            function IsJumping () {
+                                return jumping.jumping;
+                            }
+
+                            function IsSliding () {
+                                return (grounded && sliding.enabled && TooSteep());
+                            }
+
+                            function IsTouchingCeiling () {
+                                return (movement.collisionFlags & CollisionFlags.CollidedAbove) != 0;
+                            }
+
+                            function IsGrounded () {
+                                return grounded;
+                            }
+
+                            function TooSteep () {
+                                return (groundNormal.y <= Mathf.Cos(controller.slopeLimit * Mathf.Deg2Rad));
+                            }
+
+                            function GetDirection () {
+                                return inputMoveDirection;
+                            }
+
+                            function SetControllable (controllable : boolean) {
+                                canControl = controllable;
+                            }
+
+                                // Project a direction onto elliptical quater segments based on forward, sideways, and backwards speed.
+                                // The function returns the length of the resulting vector.
+                                function MaxSpeedInDirection (desiredMovementDirection : Vector3) : float {
+                                    if (desiredMovementDirection == Vector3.zero)
+                                        return 0;
+                                    else {
+                                        var zAxisEllipseMultiplier : float = (desiredMovementDirection.z > 0 ? movement.maxForwardSpeed : movement.maxBackwardsSpeed) / movement.maxSidewaysSpeed;
+                                        var temp : Vector3 = new Vector3(desiredMovementDirection.x, 0, desiredMovementDirection.z / zAxisEllipseMultiplier).normalized;
+                                        var length : float = new Vector3(temp.x, 0, temp.z * zAxisEllipseMultiplier).magnitude * movement.maxSidewaysSpeed;
+                                        return length;
+                                    }
+                                }
+
+                                    function SetVelocity (velocity : Vector3) {
+                                        grounded = false;
+                                        movement.velocity = velocity;
+                                        movement.frameVelocity = Vector3.zero;
+                                        SendMessage("OnExternalVelocity");
+                                    }
+
+                                        // Require a character controller to be attached to the same game object
+                                    @script RequireComponent (CharacterController)
+                                    @script AddComponentMenu ("Character/Character Motor")
+
