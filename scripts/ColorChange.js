ea09618f42a0316a27e2b0bc2a458b778718f2a1
Tuğba DÜZENLİ <tugbadznl@gmail.com>
2014-01-06 16:28:08 +0200

    color change

---
+++ scripts/ColorChange.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+var isQuitButton : boolean;
+var isOptionsButton : boolean;
+var isStartButton : boolean;
+
+function OnMouseEnter()
+{
+//changing color of text maouse on it
+renderer.material.color = Color.green; 
+}
+
+function OnMouseExit()
+{
+//changing color of text maouse on it
+renderer.material.color = Color.white; 
+}
+
+function OnMouseUp()
+{
+	if(isQuitButton == true)
+	{
+	//We perform exit operation here
+		Application.Quit();
+	}
+	if(isOptionsButton == true)
+	{
+	 // we go to options sceene here
+	 Application.LoadLevel("Options");
+	}
+	if(isStartButton == true)
+	{
+		//Starting game here
+		Application.LoadLevel("Sahne1");
+	}
+
+}
