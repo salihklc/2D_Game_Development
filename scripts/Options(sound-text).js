b7710c9e7d58d4e0de0e366a80d43d648b5634d4
Yasin <yas92cinar@gmail.com>
2014-01-06 16:54:37 +0200

    Last changes

---
+++ scripts/Options(sound-text).js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var isBackButton: boolean;
+
+function OnMouseEnter()
+{
+//changing color of text maouse on it
+renderer.material.color = Color.green; 
+}
+
+function OnMouseExit()
+{
+//changing color of text maouse on it
+renderer.material.color = Color.white; 
+}
+
+function OnMouseUp()
+{
+	if(isBackButton == true)
+	{
+	//Loading back to main menu
+	Application.LoadLevel("MainMenu");
+	}
+
+else
+{	//Volume mute coding
+	if(GetComponent(TextMesh).text == "ON")
+	{
+	GetComponent(TextMesh).text = "OFF";
+	 AudioListener.pause = true;
+	}
+	else
+	{
+		GetComponent(TextMesh).text = "ON";
+		 AudioListener.pause = false;
+	}
+}
+}
